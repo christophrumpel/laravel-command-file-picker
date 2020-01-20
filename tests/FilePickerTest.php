95eb9b3554ef81a2fbe15571232d721f63adb75b
Christoph Rumpel <c.rumpel@kabsi.at>
2020-01-20 16:16:44 +0100

    Refactor to register the test command inside the test

--- tests/FilePickerTest.php
+++ tests/FilePickerTest.php
@@ -2,25 +2,36 @@
 
 namespace Christophrumpel\LaravelCommandFilePicker\Tests;
 
-
+use Christophrumpel\LaravelCommandFilePicker\LaravelCommandFilePickerServiceProvider;
+use Christophrumpel\LaravelCommandFilePicker\Tests\Commands\TestCommand;
+use Illuminate\Console\Application;
 use Illuminate\Support\Facades\Artisan;
 use Orchestra\Testbench\TestCase;
 
 class FilePickerTest extends TestCase
 {
 
-    protected function getPackageProviders($app)
+    public function setUp(): void
     {
-        return [TestServiceProvider::class];
+        parent::setUp();
+
+        Application::starting(function (Application $artisan) {
+            $artisan->add(app(TestCommand::class));
+        });
     }
 
-   /** @test **/
-   public function it_uses_method_from_trait_to_ask_show_choices()
-   {
-       Artisan::call('make:model UserModel');
+    protected function getPackageProviders($app)
+    {
+        return [LaravelCommandFilePickerServiceProvider::class];
+    }
 
-       $this->artisan('run:test-command')
-           ->expectsQuestion('Please pick a class', 'file-path')
-           ->expectsOutput('Thanks. You have chosen: file-path');
-   }
+    /** @test * */
+    public function it_uses_method_from_trait_to_ask_show_choices()
+    {
+        Artisan::call('make:model UserModel');
+
+        $this->artisan('run:test-command')
+            ->expectsQuestion('Please pick a class', 'file-path')
+            ->expectsOutput('Thanks. You have chosen: file-path');
+    }
 }
