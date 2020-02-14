118146674e9cc6afa72c86db2b67db9d33a6721b
Mohammed Al-kutrani <mohammedalkutrani@gmail.com>
2020-02-14 11:34:21 +0200

    handle protected methods

--- tests/LoadingFromOtherClassesTest.php
+++ tests/LoadingFromOtherClassesTest.php
@@ -17,8 +17,17 @@ class LoadingFromOtherClassesTest extends TestCase
     /** @test */
     public function it_getting_models_as_collection()
     {
-        $result = $this->traitMock->loadModels(__DIR__.'/Data/Models')->toCollection();
-
+        $result = $this->invokeMethod($this->traitMock,'loadModels',[__DIR__.'/Data/Models']);
+        $result = $this->invokeMethod($result,'toCollection');
         $this->assertTrue($result instanceof \Illuminate\Support\Collection);
+    }
+
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
     }
 }
